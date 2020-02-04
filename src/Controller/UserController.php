56691b7feb64497be0580f0b7d66cb8a09e6246f
Spriinty <quentinpetrequin@gmail.com>
2020-02-04 12:28:34 +0000

    suppression formulaire qu'on a mis 4 heures à faire

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,15 +2,12 @@
 
 namespace App\Controller;
 
-use App\Form\VoteType;
-use App\Entity\Reponse;
-use App\Repository\UserRepository;
+use App\Entity\Emotion;
 use App\Repository\EmotionRepository;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
@@ -18,45 +15,33 @@ class UserController extends AbstractController
      * @IsGranted("ROLE_USER")
      * @Route("/user", name="user")
      */
-    public function
-    default(EmotionRepository $emotionRepository)
+    public function default(EmotionRepository $emotionRepository)
     {
         $this->denyAccessUnlessGranted('ROLE_USER');
 
-        // or add an optional message - seen by developers
-        $this->denyAccessUnlessGranted('ROLE_USER', null, 'Seul le rôle user est authorisé');
+    // or add an optional message - seen by developers
+    $this->denyAccessUnlessGranted('ROLE_USER', null, 'Seul le rôle user est authorisé');
 
-        // $user = $this->getUser();
+    // $user = $this->getUser();
 
-        $emotions = $emotionRepository->findAll();
+    $emotions = $emotionRepository->findAll();
 
         return $this->render('user/vote.html.twig', [
             'emotions' => $emotions
         ]);
     }
 
-    /**
+
+
+       /**
      * @Route("user/reponse/{id}", name="reponse")
      */
-    public function reponse(Request $request, EmotionRepository $emotionRepository, EntityManagerInterface $entityManager)
+    public function reponse(EmotionRepository $emotionRepository)
     {
-        $vote = new Reponse();
-        $form = $this->createForm(VoteType::class, $vote);
-        $form->handleRequest($request);
-
-        // Si c'est valide et soumis :
-        if ($form->isSubmitted() && $form->isValid()) {
-
-            //On insère les données :
-            $entityManager->persist($vote);
-
-            //Commit de la base de données
-            $entityManager->flush();
-
-            return $this->redirectToRoute('reponse');
-        }
         return $this->render('user/reponse.html.twig', [
             'controller_name' => 'UserController',
         ]);
     }
+
+    
 }

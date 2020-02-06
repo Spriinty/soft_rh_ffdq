c32a2223ad47158401ded00ea4a694dd20901281
DesireeBesnard <desireebesnard@gmail.com>
2020-02-06 15:50:56 +0000

    récupération donnée pour graphiques

--- src/Repository/ReponseRepository.php
+++ src/Repository/ReponseRepository.php
@@ -3,8 +3,10 @@
 namespace App\Repository;
 
 use App\Entity\Reponse;
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use App\Repository\ReponseRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 
 /**
  * @method Reponse|null find($id, $lockMode = null, $lockVersion = null)
@@ -20,13 +22,34 @@ class ReponseRepository extends ServiceEntityRepository
     }
 
     public function dailyCompanyResponse(){
+
+        $today= new \DateTime();
+        $currentdate=$today->format('Y-m-d');
+
         return $this->createQueryBuilder('r')
         ->select('COUNT(r.emotion)')
-        ->where("r.date='Y-m-d'")
+        ->where("r.date >= :today")
         ->groupby('r.emotion')
+        ->setParameter('today', $currentdate)
         ->getQuery()
         ->getResult();
     }
+
+    public function dailyServiceResponse($serviceid){
+
+        $serviceid = 
+
+        return $this->createQueryBuilder('r')
+        ->select('e.name, count(r.id) AS count')
+        ->join('r.emotion', 'e')
+        ->where('r.service = :service')
+        ->groupby('e.name')
+        ->setParameter('service', $serviceid)
+        ->getQuery()
+        ->getResult();
+    }
+
+
     // /**
     //  * @return Reponse[] Returns an array of Reponse objects
     //  */
